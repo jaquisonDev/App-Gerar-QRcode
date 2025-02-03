dae4e69d63537c8d9b30156aac20786b1ed1ece6
jaquisonDev <jaquison1000@gmail.com>
2025-02-03 11:58:29 -0300

    Adicionei a função imageClickDownload para quando clicar exibir o modal

--- JS/script.js
+++ JS/script.js
@@ -6,30 +6,7 @@ const title = document.querySelector(".title");
 const btnYes = document.getElementById("btn-yes");
 const btnNo = document.getElementById("btn-no");
 
-btnGenerater.addEventListener("click", () => {
-  if (!text.value) {
-    btnGenerater.focus();
-    btnGenerater.innerHTML = "⚠️ Você deve digitar algum texto ⚠️";
-    btnGenerater.style.backgroundColor = "#FF6347";
-    return;
-  }
-
-  let qrCode = "";
-
-  modal.style.display = "flex";
-  title.innerHTML = "Gerando QRCode...";
-  btnYes.style.display = "none";
-  btnNo.style.display = "none";
-
-  setTimeout(() => {
-    modal.style.display = "none";
-
-    let generateQrcode = `https://quickchart.io/qr?text=${text.value}`;
-    area_qrcode.src = generateQrcode;
-
-    qrCode = generateQrcode;
-  }, 5000);
-
+const imageClickDownload = (qrCode, showValue) => {
   setTimeout(() => {
     modal.style.display = "flex";
     title.innerHTML = "Você deseja fazer download?";
@@ -53,5 +30,39 @@ btnGenerater.addEventListener("click", () => {
     btnNo.addEventListener("click", () => {
       modal.style.display = "none";
     });
-  }, 6000);
+  }, showValue);
+};
+
+btnGenerater.addEventListener("click", () => {
+  if (!text.value) {
+    btnGenerater.focus();
+    btnGenerater.innerHTML = "⚠️ Você deve digitar algum texto ⚠️";
+    btnGenerater.style.backgroundColor = "#FF6347";
+    return;
+  }
+
+  let qrCode = "";
+
+  modal.style.display = "flex";
+  title.innerHTML = "Gerando QRCode...";
+  btnYes.style.display = "none";
+  btnNo.style.display = "none";
+
+  setTimeout(() => {
+    modal.style.display = "none";
+
+    let generateQrcode = `https://quickchart.io/qr?text=${text.value}`;
+    area_qrcode.src = generateQrcode;
+    qrCode = generateQrcode;
+  }, 5000);
+
+  imageClickDownload(qrCode, 6000);
+
+  area_qrcode.addEventListener("click", () => {
+    if (qrCode !== "") {
+      imageClickDownload(qrCode, 0);
+    }
+
+    return;
+  });
 });
